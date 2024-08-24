89e4b754ab0f7124f3d89c28f435237736bcab80
Vaishali69R <169047063+Vaishali69R@users.noreply.github.com>
2024-08-24 17:14:10 +0530

    Add files via upload

---
+++ Alternatearray.java
@@ -0,0 +1,9 @@
+ class Alternatearray {
+    public static void main(String[] args) {
+        int a[]={11,12,13,14,15,16,17};
+        int alter =0;
+        for(int i=0;i<a.length;i=i+2){
+            System.out.println(a[i]);
+        }
+    }
+}

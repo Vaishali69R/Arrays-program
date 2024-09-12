7250e3c5d21ace7c98f1572a43aa001fc149379b
Vaishali69R <169047063+Vaishali69R@users.noreply.github.com>
2024-09-12 23:43:03 +0530

    Add files via upload

---
+++ Jaggedarray.java
@@ -0,0 +1,13 @@
+ class Jaggedarray {
+    public static void main(String[] args) {
+        int a[][]={{1,5,7},{2,1},{5,10,11,15}};
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[i].length;j++){
+                System.out.print(a[i][j]+ " ");
+            }
+            System.out.println();
+        }
+       
+    }
+    
+}

655e07fb7e1d5efffb2b79c5bf95c0afb5aa66f0
Vaishali69R <169047063+Vaishali69R@users.noreply.github.com>
2024-09-13 23:14:51 +0530

    Add files via upload

---
+++ Foreacharray.java
@@ -0,0 +1,14 @@
+ class Foreacharray {
+    public static void main(String[] args) {
+        int a[]={10,23,12,13};
+        for(int i : a){
+            System.out.println(i);
+        }
+        String ar[] = {"vish" , "maya" , "gopi"};
+        for(String i : ar){
+            System.out.println(i);
+
+        }
+    }
+    
+}

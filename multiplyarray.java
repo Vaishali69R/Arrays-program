3398aa8255bf22ce1e39f5b163671b19ad433787
Vaishali69R <169047063+Vaishali69R@users.noreply.github.com>
2024-09-14 17:17:52 +0530

    Add files via upload

---
+++ multiplyarray.java
@@ -0,0 +1,12 @@
+ class multiplyarray {
+    public static void main(String[] args) {
+        int a[]={3,5,6,7,8};
+        int mult=1;
+        for(int i=0;i<a.length;i++){
+            mult=mult*a[i];
+        }
+                System.out.println(mult);        
+        }
+    }
+    
+

a9a8560c09930560d796968035cfc1703cdcadd6
Vaishali69R <169047063+Vaishali69R@users.noreply.github.com>
2024-10-11 13:49:28 +0530

    Add files via upload

--- Swaparray.java
+++ Swaparray.java
@@ -6,7 +6,7 @@
         a[0]=  a[a.length-1];
         a[a.length-1] = temp;
         for(int i=0; i<a.length;i++){
-            System.out.println(a[i]);
+            System.out.println(temp);
         }
     }    
 }
